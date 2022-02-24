dbd70ed6588d759d8ebb92dfa840f4dce9eb51e7
choshsh <cho911115@gmail.com>
2022-02-24 22:20:12 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+// Import Main styles for this application
+@import 'styles/style';
+@import '~ag-grid-community/dist/styles/ag-grid.css';
+@import '~ag-grid-community/dist/styles/ag-theme-alpine.css';
+</style>
